1140946f41d5914388c8f588d00967236935daab
Milan Carati <37277@ma-web.nl>
2023-10-16 12:03:09 +0200

    milans changes

--- js/main.js
+++ js/main.js
@@ -9,7 +9,10 @@ function basicHaiku()
     return ["Toward those short trees","Olgun is better dan milan 0_0","On a day in spring."]
 }
 //zet hier je haiku functie neer, zie https://github.com/progsen/haikugitopdracht voor ideeen
-
+function student1Haiku ()
+{
+    return ["No one travels Along this way but I, This autumn evening.", "In the twilight rain these brilliant-hued hibiscus - A lovely sunset","The wren Earns his living Noiselessly."]
+}
 haikus = [
     basicHaiku()
 ]
